685deeca218573faad6e7160d420b58935d310c9
moriy <moriyc@post.bgu.ac.il>
2025-12-05 12:30:22 +0200

    mor

--- isoKnight/alpha_beta_isoKnight.py
+++ isoKnight/alpha_beta_isoKnight.py
@@ -3,36 +3,44 @@ import math
 
 def alphabeta_max(current_game):
     #add code here for alpha-beta
-    pass
+    return maximin(current_game,-math.inf,math.inf)
+
+
 
 def alphabeta_min(current_game):
     #add code here for alpha-beta
-    pass
+    return minimax(current_game, -math.inf, math.inf)
 
 
-def maximin(current_game):
+def maximin(current_game,alpha,beta):
     if current_game.is_terminal():
         return current_game.get_score(), None
     v = -math.inf
     moves = current_game.get_moves()
     for move in moves:
-        mx, next_move = minimax(move)
+        mx, next_move = minimax(move,alpha,beta)
         if v < mx:
             v = mx
             best_move = move
         #add code here for alpha-beta algorithm
+        alpha=max(alpha,v)
+        if alpha>=beta:
+            return v,None
     return v, best_move
 
 
-def minimax(current_game):
+def minimax(current_game,alpha,beta):
     if current_game.is_terminal():
         return current_game.get_score(), None
     v = math.inf
     moves = current_game.get_moves()
     for move in moves:
-        mx, next_move = maximin(move)
+        mx, next_move = maximin(move,alpha,beta)
         if v > mx:
             v = mx
             best_move = move
         #add code here for alpha-beta algorithm
+        beta = min(beta, v)
+        if alpha >= beta:
+            return v, None
     return v, best_move
