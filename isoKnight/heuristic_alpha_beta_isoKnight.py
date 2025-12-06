e2eda38af4a726bc5d29f2ebdfe6dc6227309d25
avrshay <shaymilo23@gmail.com>
2025-12-06 21:09:29 +0200

    minimax, maximin

--- isoKnight/heuristic_alpha_beta_isoKnight.py
+++ isoKnight/heuristic_alpha_beta_isoKnight.py
@@ -1,4 +1,5 @@
 import math
+
 h = None
 
 
@@ -6,6 +7,7 @@ def alphabeta_max_h(current_game, _heuristic, depth=3):
     global h
     h = _heuristic
     # add code here
+
     pass
 
 
@@ -13,10 +15,11 @@ def alphabeta_min_h(current_game, _heuristic, depth=3):
     global h
     h = _heuristic
     # add code here
+
     pass
 
 
-def maximin(current_game, depth):
+def maximin(current_game, depth, alpha, beta):
     global h
     if current_game.is_terminal():
         return current_game.get_score(), None
@@ -25,25 +28,37 @@ def maximin(current_game, depth):
     v = -math.inf
     moves = current_game.get_moves()
     for move in moves:
-        mx, next_move = minimax(move, depth - 1)
+        mx, next_move = minimax(move, depth - 1, alpha, beta)
         if v < mx:
             v = mx
             best_move = move
+
+        alpha = max(v, alpha)
+        if beta <= alpha:
+            break  # dont choose this way
+
     return v, best_move
 
 
-def minimax(current_game, depth):
+def minimax(current_game, depth, alpha, beta):
     global h
     if current_game.is_terminal():
-        return current_game.get_score(), None
+        return current_game.get_score(), None  # stop condition
     if depth == 0:
-        return h(current_game), None
+        return h(
+            current_game), None  # reach to the maximum search depth -Time and memory limit, so we return the v=h instead
     v = math.inf
     moves = current_game.get_moves()
+    best_move = None
+
     for move in moves:
-        mx, next_move = maximin(move, depth - 1)
+        mx, next_move = maximin(move, depth - 1, alpha, beta)
         if v > mx:
             v = mx
             best_move = move
+
+        beta = min(v, beta)
+        if beta <= alpha:  # there is no point in continuing this path:
+            break  # don't choose this way
 
     return v, best_move
